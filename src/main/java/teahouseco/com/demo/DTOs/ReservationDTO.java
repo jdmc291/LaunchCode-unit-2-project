6e3124aac90c84f3e479606cea13c6dd3b0018be
Jalen McBride <jamcbride131@gmail.com>
2025-11-06 09:48:50 -0600

    Created ReservationDTO for Post request

---
+++ src/main/java/teahouseco/com/demo/DTOs/ReservationDTO.java
@@ -0,0 +1,31 @@
+package teahouseco.com.demo.DTOs;
+
+public class ReservationDTO {
+    public int userId;
+    public int eventId;
+    public String additionalInfo;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+}
