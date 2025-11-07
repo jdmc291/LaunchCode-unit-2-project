0eef29d5a6e56885df556c984f3f77969572f54e
Jalen McBride <jamcbride131@gmail.com>
2025-11-06 19:38:55 -0600

    Changed events controller to use EventService class instead of EventRepository

--- src/main/java/teahouseco/com/demo/Controllers/EventsController.java
+++ src/main/java/teahouseco/com/demo/Controllers/EventsController.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
+import teahouseco.com.demo.DTOs.ReservationDTO;
 import teahouseco.com.demo.Models.Event;
+import teahouseco.com.demo.Repositories.AttendingRepository;
 import teahouseco.com.demo.Repositories.EventRepository;
+import teahouseco.com.demo.Services.AttendingService;
+import teahouseco.com.demo.Services.EventService;
 
 import java.util.List;
 
@@ -17,15 +21,29 @@ import java.util.List;
 public class EventsController {
 
     @Autowired
-    EventRepository eventRepository;
+    EventService eventService;
+
+    @Autowired
+    AttendingService attendingService;
+
+//    @Autowired
+//    AttendingRepository attendingRepository;
 
     @GetMapping("/getAllEvents")
     public ResponseEntity<List<Event>> getAllEvents() {
         try{
-            return new ResponseEntity<>(eventRepository.findAll(), HttpStatusCode.valueOf(200));
+            return new ResponseEntity<>(eventService.getAllEvents(), HttpStatusCode.valueOf(200));
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatusCode.valueOf(500));
         }
+
+    }
+
+    @PostMapping("/createReservation")
+    public ResponseEntity<Event> createEvent(@RequestBody ReservationDTO myReservationDto) {
+
+        attendingService.AddReservation(myReservationDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
 }
