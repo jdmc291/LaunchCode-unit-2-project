c56e0f910fc82f07c6f2afc0b4e2f533e744087c
Jalen McBride <jamcbride131@gmail.com>
2025-11-06 19:35:28 -0600

    Added more fields in ReservationDTO

--- src/main/java/teahouseco/com/demo/DTOs/ReservationDTO.java
+++ src/main/java/teahouseco/com/demo/DTOs/ReservationDTO.java
@@ -3,10 +3,19 @@ package teahouseco.com.demo.DTOs;
 public class ReservationDTO {
     public int userId;
     public int eventId;
+    public int number_of_guests;
     public String additionalInfo;
 
     public int getUserId() {
         return userId;
+    }
+
+    public int getNumber_of_guests() {
+        return number_of_guests;
+    }
+
+    public void setNumber_of_guests(int number_of_guests) {
+        this.number_of_guests = number_of_guests;
     }
 
     public void setUserId(int userId) {
