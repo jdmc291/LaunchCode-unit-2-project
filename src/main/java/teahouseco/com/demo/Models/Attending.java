c3513903e78118bee8ac7e72b0b4a4e51070c6cf
Jalen McBride <jamcbride131@gmail.com>
2025-11-06 19:34:51 -0600

    Added setters and getters to attending class

--- src/main/java/teahouseco/com/demo/Models/Attending.java
+++ src/main/java/teahouseco/com/demo/Models/Attending.java
@@ -23,5 +23,44 @@ public class Attending {
     public int number_of_guests;
     public String additional_info;
 
+    public AttendingIds getId() {
+        return id;
+    }
+
+    public void setId(AttendingIds id) {
+        this.id = id;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public int getNumber_of_guests() {
+        return number_of_guests;
+    }
+
+    public void setNumber_of_guests(int number_of_guests) {
+        this.number_of_guests = number_of_guests;
+    }
+
+    public String getAdditional_info() {
+        return additional_info;
+    }
+
+    public void setAdditional_info(String additional_info) {
+        this.additional_info = additional_info;
+    }
 }
 
