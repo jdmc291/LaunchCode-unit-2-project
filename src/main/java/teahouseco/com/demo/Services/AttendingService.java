04f781e4dcb05b9d8699673e3e31241033cdf8c2
Jalen McBride <jamcbride131@gmail.com>
2025-11-08 11:44:45 -0600

    Created Delete reservation call

--- src/main/java/teahouseco/com/demo/Services/AttendingService.java
+++ src/main/java/teahouseco/com/demo/Services/AttendingService.java
@@ -88,4 +88,26 @@ public class AttendingService {
 
         attendingRepository.save(updatedAttendingObject);
     }
-}
+
+    public void RemoveReservation(ReservationDTO EnteredReservation) {
+        Attending updatedAttendingObject = new Attending();
+        AttendingIds updatedIds = new AttendingIds();
+        updatedIds.setCustomer_id(EnteredReservation.getUserId());
+        updatedIds.setEvent_id(EnteredReservation.getEventId());
+
+        updatedAttendingObject.setId(updatedIds);
+
+        User referencedUser = findUserById(EnteredReservation.getUserId());
+        Event referencedEvent = findEventById(EnteredReservation.getEventId());
+
+        updatedAttendingObject.setCustomer(referencedUser);
+        updatedAttendingObject.setEvent(referencedEvent);
+        updatedAttendingObject.number_of_guests =
+                EnteredReservation.getNumber_of_guests();
+        updatedAttendingObject.additional_info =
+                EnteredReservation.getAdditionalInfo();
+
+        attendingRepository.delete(updatedAttendingObject);
+    }
+
+    }
