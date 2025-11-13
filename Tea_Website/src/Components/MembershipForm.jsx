d678f3dde372c132f7e246af1fef6d9322150b57
Jalen McBride <jamcbride131@gmail.com>
2025-11-12 19:19:16 -0600

    I changed the membership form to  have users sign in instead of register

--- Tea_Website/src/Components/MembershipForm.jsx
+++ Tea_Website/src/Components/MembershipForm.jsx
@@ -1,44 +1,70 @@
 import { useState } from "react";
 import TeaHouseCloseButton from "./TeaHouseCloseButton";
+import LoginRequest from "../APICalls/AuthenticationCalls";
+import {jwtDecode} from "jwt-decode"
 
 const MembershipForm = (props) => {
 
     const { state, setState } = props;
 
+    const [loginAttempt, setLoginAttempt] = useState(false);
+    const [loginSuccessful, setLoginSuccessful] = useState(false);
+    const [submitMessage, setSubmitMessage] = useState("");
+
     const [firstName, setFirstName] = useState("");
     const [lastname, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [address, setAddress] = useState("");
-    const [submitMessage, setSubmitMessage] = useState("");
+    const [password, setPassword] = useState("");
+
 
     //Form Submitted Successfully. Check your email's inbox &#9989;
 
-    const handleSubmission = (e) => {
-        
+    const handleSubmission = async (e) => {
+
         //Prevents the form from going away on submit
         e.preventDefault();
 
         // This will check if their inputs meet what is required
 
+        const credentials = {
 
-        if (firstName.length < 3 || lastname.length < 3) {
+            email: email,
+            password: password
 
-            setSubmitMessage("Error on Form. Please Make First and Last Name Longer");
-        } else if (!email.includes("@gmail.com") && !email.includes("@icloud.com") && !email.includes("@yahoo.com")) {
-
-            setSubmitMessage("Error on Form. Please enter iCloud, GMail, or Yahoo email");
-
-        } else {
-
-            setSubmitMessage(`Form Submitted Successfully. Check your email's inbox`)
         }
+
+        fetch('http://localhost:8080/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        }).then(response => {
+
+            if (!response.ok) {
+                setSubmitMessage("Username or Password is incorrect")
+            }
+            else {
+
+                setSubmitMessage("Login successful");
+
+            }
+
+            return response.json();
+        }).then(data => {
+            localStorage.setItem("TeaHouseToken", data.token);
+
+        })
+
+
     }
 
     //tracking what the user puts in the values
 
     const handleFirstNameChange = (e) => {
         let currentFirstName = e.target.value.toLowerCase();
-        
+
         setFirstName(currentFirstName);
 
     }
@@ -51,8 +77,8 @@ const MembershipForm = (props) => {
         setEmail(e.target.value.toLowerCase());
     }
 
-    const handleAddressChange = (e) => {
-        setAddress(e.target.value.toLowerCase());
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value.toLowerCase());
     }
 
     return (
@@ -65,24 +91,18 @@ const MembershipForm = (props) => {
                             <>
                                 <TeaHouseCloseButton setSwitch={setState} id="member-close-button" />
 
-                                <h1 id="become-member-title">Member Registration</h1>
+                                <h1 id="become-member-title">Sign Into Your Account</h1>
                                 <div id="content-box">
                                     <div id="member-registration-area">
                                         <form id="form-element" onSubmit={(e) => handleSubmission(e)} >
-                                            <label className="member-labels" id="sign-up-firstName-label" htmlFor="sign-up-firstName" >First Name: </label>
-                                            <input className="member-input" id="sign-up-firstName" type="text" placeholder="John" required onChange={(e) => handleFirstNameChange(e)} />
-                                            <br></br>
-
-                                            <label className="member-labels" htmlFor="sign-up-lastName">Last Name: </label>
-                                            <input className="member-input" id="sign-up-lastName" type="text" placeholder="Smith" required onChange={(e) => handleLastNameChange(e)} />
 
                                             <br></br>
                                             <label className="member-labels" htmlFor="sign-up-email">Email: </label>
                                             <input className="member-input" id="sign-up-email" type="email" placeholder="theteahouse@company.com" required onChange={(e) => handleEmailChange(e)} />
                                             <br></br>
 
-                                            <label className="member-labels" htmlFor="sign-up-address">Address: </label>
-                                            <input className="member-input" id="sign-up-address" type="text" placeholder="123 The Teahouse St." required onChange={(e) => handleAddressChange(e)} />
+                                            <label className="member-labels" htmlFor="password-input">Password: </label>
+                                            <input className="member-input" id="password-input" type="password" placeholder="Enter Your Password" required onChange={(e) => handlePasswordChange(e)} />
 
                                             <br></br>
                                             <br></br>
